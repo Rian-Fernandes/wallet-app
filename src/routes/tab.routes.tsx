ebe8b7585804b6e5c028a271a5a22e6e610abbb7
Rian-Fernandes <rian10fernandes@hotmail.com>
2024-03-13 16:26:19 -0300

    Configurando e estilizando tabbar

--- src/routes/tab.routes.tsx
+++ src/routes/tab.routes.tsx
@@ -2,12 +2,15 @@ import { View, Text } from "react-native";
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { AntDesign } from "@expo/vector-icons";
-
+import { Entypo } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
 import theme from "@src/styles/theme";
 import { Carteira } from "@src/screens/Tab/Carteira";
 import { Notificacao } from "@src/screens/Tab/Notificacao";
 import { Relatorio } from "@src/screens/Tab/Relatorio";
 import { Settings } from "@src/screens/Tab/Settings";
+import { Fontisto } from "@expo/vector-icons";
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
@@ -36,9 +39,15 @@ export const TabRoutes = () => {
                 options={{
                     tabBarLabel: "Home",
                     tabBarIcon: ({ focused }) => (
-                        <View>
-                            <AntDesign
-                                name="creditcard"
+                        <View
+                            style={{
+                                marginTop: 3,
+                                alignItems: "center",
+                                justifyContent: "center",
+                            }}
+                        >
+                            <Entypo
+                                name="wallet"
                                 size={24}
                                 color={
                                     focused
@@ -50,9 +59,84 @@ export const TabRoutes = () => {
                     ),
                 }}
             />
-            <Screen name="Relatorio" component={Relatorio} />
-            <Screen name="Notificacao" component={Notificacao} />
-            <Screen name="Settings" component={Settings} />
+            <Screen
+                name="Relatorio"
+                component={Relatorio}
+                options={{
+                    tabBarLabel: "Relatório",
+                    tabBarIcon: ({ focused }) => (
+                        <View
+                            style={{
+                                marginTop: 3,
+                                alignItems: "center",
+                                justifyContent: "center",
+                            }}
+                        >
+                            <MaterialIcons
+                                name="insert-chart"
+                                size={24}
+                                color={
+                                    focused
+                                        ? theme.COLORS.BLACK
+                                        : theme.COLORS.GRAY5
+                                }
+                            />
+                        </View>
+                    ),
+                }}
+            />
+            <Screen
+                name="Notificacao"
+                component={Notificacao}
+                options={{
+                    tabBarLabel: "Notificação",
+                    tabBarIcon: ({ focused }) => (
+                        <View
+                            style={{
+                                marginTop: 3,
+                                alignItems: "center",
+                                justifyContent: "center",
+                            }}
+                        >
+                            <MaterialCommunityIcons
+                                name="bell"
+                                size={24}
+                                color={
+                                    focused
+                                        ? theme.COLORS.BLACK
+                                        : theme.COLORS.GRAY5
+                                }
+                            />
+                        </View>
+                    ),
+                }}
+            />
+            <Screen
+                name="Settings"
+                component={Settings}
+                options={{
+                    tabBarLabel: "Configuração",
+                    tabBarIcon: ({ focused }) => (
+                        <View
+                            style={{
+                                marginTop: 3,
+                                alignItems: "center",
+                                justifyContent: "center",
+                            }}
+                        >
+                            <Fontisto
+                                name="player-settings"
+                                size={24}
+                                color={
+                                    focused
+                                        ? theme.COLORS.BLACK
+                                        : theme.COLORS.GRAY5
+                                }
+                            />
+                        </View>
+                    ),
+                }}
+            />
         </Navigator>
     );
 };

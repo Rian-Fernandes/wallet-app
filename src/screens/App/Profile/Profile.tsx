4889e3c93ae7aa1906dbb42ad7f1ec9fa73dd9da
Rian-Fernandes <rian10fernandes@hotmail.com>
2024-03-19 12:59:24 -0300

    Finalizando perfil do usuario

--- src/screens/App/Profile/Profile.tsx
+++ src/screens/App/Profile/Profile.tsx
@@ -1,14 +1,71 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
-import { Container } from "./styles";
+import { Configs } from "@src/components/Configs";
+import { Octicons } from "@expo/vector-icons";
+import {
+    Container,
+    Header,
+    Content,
+    Footer,
+    Avatar,
+    ViewHeader,
+    HeaderName,
+    IconButton,
+    ViewFooter,
+    ViewIconButton,
+    TitleFooter,
+} from "./styles";
 import { GoBack } from "@src/components/GoBack";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { useTheme } from "styled-components";
 
 export const Profile = () => {
+    const { COLORS } = useTheme();
     return (
         <>
             <GoBack />
             <Container>
-                <Text>Profile</Text>
+                <Header>
+                    <Avatar
+                        source={{
+                            uri: "https://t4.ftcdn.net/jpg/03/83/25/83/360_F_383258331_D8imaEMl8Q3lf7EKU2Pi78Cn0R7KkW9o.jpg",
+                        }}
+                    />
+                    <ViewHeader>
+                        <HeaderName>Rian F.</HeaderName>
+                        <IconButton>
+                            <FontAwesome5
+                                name="edit"
+                                size={20}
+                                color={COLORS.GRAY2}
+                            />
+                        </IconButton>
+                    </ViewHeader>
+                </Header>
+                <Content>
+                    <Configs name="Conta Conectada" onPress={() => {}} />
+                    <Configs
+                        name="Privacidade e Segurança"
+                        onPress={() => {}}
+                    />
+                    <Configs name="Configuração de Login" onPress={() => {}} />
+                    <Configs name="Centro de Serviço" onPress={() => {}} />
+                </Content>
+                <Footer>
+                    <ViewFooter>
+                        <TouchableOpacity>
+                            <ViewIconButton>
+                                <Octicons
+                                    name="trash"
+                                    size={36}
+                                    color={COLORS.PURPLECARD}
+                                />
+                            </ViewIconButton>
+                        </TouchableOpacity>
+
+                        <TitleFooter>Log out</TitleFooter>
+                    </ViewFooter>
+                </Footer>
             </Container>
         </>
     );
--- src/screens/App/Profile/styles.ts
+++ src/screens/App/Profile/styles.ts
@@ -1,3 +1,59 @@
 import styled from "styled-components/native";
 
-export const Container = styled.View``;
+export const Container = styled.View`
+    padding: 5px;
+`;
+
+export const Header = styled.View`
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+`;
+export const Content = styled.View`
+    margin-top: 20px;
+`;
+
+export const Footer = styled.View`
+    margin-top: 10px;
+`;
+
+export const Avatar = styled.Image`
+    width: 96px;
+    height: 96px;
+    border-radius: 48px;
+`;
+export const ViewHeader = styled.View`
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    margin-top: 5px;
+`;
+export const HeaderName = styled.Text`
+    font-size: 20px;
+    margin-right: 15px;
+    color: ${({ theme }) => theme.COLORS.BLACK};
+    font-family: ${({ theme }) => theme.FONTS.POPPINSMEDIUM};
+`;
+
+export const IconButton = styled.TouchableOpacity``;
+
+export const ViewFooter = styled.View`
+    margin-top: 30px;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+`;
+
+export const TitleFooter = styled.Text`
+    font-size: 17px;
+    color: ${({ theme }) => theme.COLORS.PURPLECARD};
+    font-family: ${({ theme }) => theme.FONTS.POPPINSMEDIUM};
+    margin-top: 5px;
+`;
+
+export const ViewIconButton = styled.View`
+    padding: 10px;
+    border-width: 1px;
+    border-color: ${({ theme }) => theme.COLORS.GRAY5};
+    border-radius: 30px;
+`;

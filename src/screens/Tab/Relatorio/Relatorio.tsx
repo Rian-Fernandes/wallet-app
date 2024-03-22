6411f0af4a4ec40a6161e93b980d17a7a9abcf03
Rian-Fernandes <rian10fernandes@hotmail.com>
2024-03-22 13:41:11 -0300

    componentizando tela de relatório (necessita ajustes)

--- src/screens/Tab/Relatorio/Relatorio.tsx
+++ src/screens/Tab/Relatorio/Relatorio.tsx
@@ -1,7 +1,26 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import React from "react";
+import { transaction } from "@src/utils/transaction";
 import { Header } from "@src/components/Header/Header";
-import { Container, BalanceImage, AmoutValue } from "./styles";
+import {
+    Container,
+    BalanceImage,
+    AmoutValue,
+    ViewContent,
+    TitleAmount,
+    ValorTotal,
+    Footer,
+    ContentFlat,
+    ContentFlatHeader,
+    Title,
+    ButtonVerTodos,
+    ButtonTitleVerTodos,
+    IconTransaction,
+    DetailsTransaction,
+    NameTransaction,
+    SubTitleTransaction,
+    PrieceTransaction,
+} from "./styles";
 import BalancePng from "@src/assets/estatistica.png";
 
 export const Relatorio = () => {
@@ -12,6 +31,44 @@ export const Relatorio = () => {
                 <BalanceImage source={BalancePng} resizeMode="contain">
                     <AmoutValue>R$2100,00</AmoutValue>
                 </BalanceImage>
+                <ViewContent>
+                    <TitleAmount>Valor Total</TitleAmount>
+                    <ValorTotal>R$12.425,00</ValorTotal>
+                </ViewContent>
+                <Footer>
+                    <FlatList
+                        data={transaction}
+                        renderItem={({ item }) => (
+                            <ContentFlat>
+                                <IconTransaction source={item.icon} />
+
+                                <DetailsTransaction>
+                                    <NameTransaction>
+                                        {item.title}
+                                    </NameTransaction>
+                                    <SubTitleTransaction>
+                                        {item.subtitle}
+                                    </SubTitleTransaction>
+                                </DetailsTransaction>
+
+                                <PrieceTransaction>
+                                    R$ {item.price}
+                                </PrieceTransaction>
+                            </ContentFlat>
+                        )}
+                        ListHeaderComponent={
+                            <ContentFlatHeader>
+                                <Title>Minhas transações</Title>
+                                <ButtonVerTodos>
+                                    <ButtonTitleVerTodos>
+                                        Mais recentes
+                                    </ButtonTitleVerTodos>
+                                </ButtonVerTodos>
+                            </ContentFlatHeader>
+                        }
+                        showsVerticalScrollIndicator={false}
+                    />
+                </Footer>
             </Container>
         </>
     );
--- src/screens/Tab/Relatorio/styles.ts
+++ src/screens/Tab/Relatorio/styles.ts
@@ -8,6 +8,8 @@ export const Container = styled.View`
 export const BalanceImage = styled.ImageBackground`
     width: 100%;
     height: 215px;
+
+    margin-top: -20px;
 `;
 
 export const AmoutValue = styled.Text`
@@ -17,4 +19,84 @@ export const AmoutValue = styled.Text`
     left: 132px;
     color: ${({ theme }) => theme.COLORS.WHITE};
     font-family: ${({ theme }) => theme.FONTS.POPPINSBOLD};
+`;
+
+export const ViewContent = styled.View`
+    /* padding: 30px; */
+    margin-top: 10px;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const TitleAmount = styled.Text`
+    font-size: 16px;
+    line-height: 20px;
+    color: ${({ theme }) => theme.COLORS.GRAY3};
+    font-family: ${({ theme }) => theme.FONTS.POPPINSMEDIUM};
+`;
+
+export const ValorTotal = styled.Text`
+    font-size: 30px;
+    line-height: 42px;
+    color: ${({ theme }) => theme.COLORS.PURPLE};
+    font-family: ${({ theme }) => theme.FONTS.POPPINSBOLD};
+`;
+
+export const Footer = styled.View`
+    flex: 1;
+    /* background-color: tomato; */
+`;
+
+export const ContentFlat = styled.View`
+    padding: 0 20px;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+export const ContentFlatHeader = styled.View`
+    padding: 10px;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+export const Title = styled.Text`
+    margin-left: 20px;
+    font-size: 20px;
+    color: ${({ theme }) => theme.COLORS.GRAY1};
+    font-family: ${({ theme }) => theme.FONTS.POPPINSMEDIUM};
+`;
+export const ButtonVerTodos = styled.TouchableOpacity``;
+
+export const ButtonTitleVerTodos = styled.Text`
+    margin-right: 10px;
+    font-size: 14px;
+    color: ${({ theme }) => theme.COLORS.PINKTEXT};
+    font-family: ${({ theme }) => theme.FONTS.POPPINSMEDIUM};
+`;
+
+export const IconTransaction = styled.Image`
+    width: 60px;
+    height: 60px;
+`;
+
+export const DetailsTransaction = styled.View`
+    flex: 1;
+`;
+
+export const NameTransaction = styled.Text`
+    font-size: 16px;
+    color: ${({ theme }) => theme.COLORS.BLACK};
+    font-family: ${({ theme }) => theme.FONTS.POPPINSMEDIUM};
+`;
+
+export const SubTitleTransaction = styled.Text`
+    font-size: 14px;
+    color: ${({ theme }) => theme.COLORS.GRAY3};
+    font-family: ${({ theme }) => theme.FONTS.POPPINSLIGHT};
+`;
+export const PrieceTransaction = styled.Text`
+    font-size: 14px;
+    color: ${({ theme }) => theme.COLORS.BLACK};
+    font-family: ${({ theme }) => theme.FONTS.POPPINSMEDIUM};
 `;

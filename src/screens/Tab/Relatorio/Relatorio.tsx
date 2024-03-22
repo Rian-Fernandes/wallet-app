886aa168d9136156c75959eb5dd17d93fd6aa30a
Rian-Fernandes <rian10fernandes@hotmail.com>
2024-03-22 13:01:17 -0300

    Iniciando tela de relatório

--- src/screens/Tab/Relatorio/Relatorio.tsx
+++ src/screens/Tab/Relatorio/Relatorio.tsx
@@ -1,13 +1,18 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Header } from "@src/components/Header/Header";
-import { Container } from "./styles";
+import { Container, BalanceImage, AmoutValue } from "./styles";
+import BalancePng from "@src/assets/estatistica.png";
 
 export const Relatorio = () => {
     return (
         <>
             <Header appName="Relatório" />
-            <Container></Container>
+            <Container>
+                <BalanceImage source={BalancePng} resizeMode="contain">
+                    <AmoutValue>R$2100,00</AmoutValue>
+                </BalanceImage>
+            </Container>
         </>
     );
 };
--- src/screens/Tab/Relatorio/styles.ts
+++ src/screens/Tab/Relatorio/styles.ts
@@ -2,7 +2,19 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
     flex: 1;
+    padding: 15px;
+`;
 
-    align-items: center;
-    justify-content: center;
+export const BalanceImage = styled.ImageBackground`
+    width: 100%;
+    height: 215px;
+`;
+
+export const AmoutValue = styled.Text`
+    font-size: 11px;
+    position: absolute;
+    top: 60px;
+    left: 132px;
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    font-family: ${({ theme }) => theme.FONTS.POPPINSBOLD};
 `;
